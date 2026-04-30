a56e2e34214bdf12f83e6c82a9b12b04ec187ff2
br1ansouza <briandesouza1597@gmail.com>
2026-04-30 20:42:24 -0300

    fix: update CORS for local network and add dotenv-rails

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL", "http://localhost:5173")
+    origins(
+      ENV.fetch("FRONTEND_URL", "http://localhost:5173"),
+      "http://localhost:4173",
+      /\Ahttp:\/\/192\.168\.\d+\.\d+:\d+\z/
+    )
 
     resource "*",
       headers: :any,
